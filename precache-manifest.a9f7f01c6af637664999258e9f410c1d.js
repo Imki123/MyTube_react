ef2597bba32144f39a490a27846a604723b2fafb
kiyoung <popping2606@gmail.com>
2020-08-22 22:20:37 +0900

    Updates

---
+++ precache-manifest.a9f7f01c6af637664999258e9f410c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5909302415c346469badb4916e6027d",
+    "url": "/MyTube_react/index.html"
+  },
+  {
+    "revision": "c1283ba9a54bdcd022e5",
+    "url": "/MyTube_react/static/css/main.e53975ae.chunk.css"
+  },
+  {
+    "revision": "c28e62c964d22e4afdf9",
+    "url": "/MyTube_react/static/js/2.2f84f910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyTube_react/static/js/2.2f84f910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1283ba9a54bdcd022e5",
+    "url": "/MyTube_react/static/js/main.6657cc1f.chunk.js"
+  },
+  {
+    "revision": "bb713c0a1c5be6e94041",
+    "url": "/MyTube_react/static/js/runtime-main.66940108.js"
+  }
+]);
