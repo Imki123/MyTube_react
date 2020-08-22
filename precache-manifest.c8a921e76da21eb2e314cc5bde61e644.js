bcea2ddd5549ffc1eeb4c446cc33eaf50c8ee7ce
kiyoung <popping2606@gmail.com>
2020-08-22 22:58:15 +0900

    Updates

---
+++ precache-manifest.c8a921e76da21eb2e314cc5bde61e644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d403817d17e0a1d58afcaadc91be334",
+    "url": "/MyTube_react/index.html"
+  },
+  {
+    "revision": "4a9cba19f49247195a82",
+    "url": "/MyTube_react/static/css/main.e53975ae.chunk.css"
+  },
+  {
+    "revision": "c28e62c964d22e4afdf9",
+    "url": "/MyTube_react/static/js/2.2f84f910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyTube_react/static/js/2.2f84f910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9cba19f49247195a82",
+    "url": "/MyTube_react/static/js/main.d85280c7.chunk.js"
+  },
+  {
+    "revision": "bb713c0a1c5be6e94041",
+    "url": "/MyTube_react/static/js/runtime-main.66940108.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ccf2256a2a6475a64211129eb4c0d2f.js"
+  "/MyTube_react/precache-manifest.c8a921e76da21eb2e314cc5bde61e644.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyTube_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
